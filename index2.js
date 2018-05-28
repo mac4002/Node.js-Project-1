7cdc73fab17d8e364d768aa6dfe3df06443ec3d2
mac4002 <omenemac@outlook.com>
2018-05-28 14:37:13 +0200

    o

--- index2.js
+++ index2.js
@@ -1,10 +1,16 @@
-const module1 = require('module1');
+const calculatrice = require('calculatrice');
 let http = require('http');
 
 let server = http.createServer((request, response) => {
     console.log('Requète vers le serveur');
     response. write('<h1>Hello 3 !</h1>');
     response. write('<h2>Fine ?</h2>');
+
+    const resultat = calculatrice.nana(12,23,42,23,42,23,42,33);
+    console.log('Resultat : ' + resultat);
+    response. write('<h2> Resultat : ' + resultat + '</h2>');
+    response. write(`<h2> Resultat : ${resultat}</h2>`);
+    
     response. end();
     });
     server.listen(3000);
---
+++ node_modules/module1/task1.js
@@ -0,0 +1,1 @@
+console.log('Chargement du du task 1');
